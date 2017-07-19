8da49f0ad4dcf91106bdf5fcf86b5dcf9c009bd4
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-19 02:06:26 +0000

    added famous_people_id

---
+++ assignments/migrations/20170719013037_milestones_with_fpid.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(t){
+        t.integer('famous_people_id').unsigned();
+        t.foreign('famous_people_id').references('famous_people.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(t){
+        t.dropColumn('famous_people_id')
+    })
+  ])
+};
